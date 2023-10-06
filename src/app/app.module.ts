815d5a38ecaea8b7898dcca5efc383db778adc4d
WlanKasper <wlankasper@gmail.com>
2023-10-06 15:32:11 +0200

    Base pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { LibraryComponent } from './pages/library/library.component';
+import { TreeComponent } from './pages/tree/tree.component';
+import { NodeComponent } from './pages/node/node.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LibraryComponent,
+    TreeComponent,
+    NodeComponent
   ],
   imports: [
     BrowserModule,
